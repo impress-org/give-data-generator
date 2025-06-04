2f1d9cb8f3968dafb1e0b22687fbac6a850d119f
Jon Waldstein <jonwaldstein@mac.mynetworksettings.com>
2025-06-04 10:43:54 -0400

    fix: settings link

--- src/Addon/Links.php
+++ src/Addon/Links.php
@@ -14,7 +14,7 @@ class Links
         $newActions = array(
             'settings' => sprintf(
                 '<a href="%1$s">%2$s</a>',
-                admin_url('edit.php?post_type=give_forms&page=give-settings&tab=give-data-generator'),
+                admin_url('edit.php?post_type=give_forms&page=data-generator'),
                 __('Settings', 'give-data-generator')
             ),
         );
